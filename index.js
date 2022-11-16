319f0e471708c6ce736e50e2031272ac29a91cbe
WM29527 <72552780+WM29527@users.noreply.github.com>
2022-11-16 22:56:27 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+
+var students = new Array("images\\\\Mr Patrick Oxley.jfif","images\\Mr Charlie O'Connell.jfif","images\\Mr Hugo Brookman.jfif","images\\Mr Edward Simpson.jfif","images\\Mr Hugh Murray.jfif","images\\Mr Jack Danielson.jfif","images\\Mr Bailey Petersen.jfif","images\\Mr Jack Ryan.jfif","images\\Mr Joshua Scott.jfif","images\\Mr Louis Beatty.jfif","images\\Mr Tom Dunning.jfif","images\\Mr Rory Bliss.jfif","images\\Mr Max Campbell.jfif","images\\Mr Harry Cool.jfif","images\\Mr Benjamin Worthington.jfif","images\\Mr Archie Cameron.jfif","images\\Mr Owen Wishart.jfif","images\\Mr Joel Humphrey.jfif","images\\Mr Wilson Slack.jfif","images\\Mr George Pearce Mann.jfif","images\\Mr Daniel Wallis.jfif","images\\Mr Joseph Oxenford.jfif","images\\Mr William McNally.jfif","images\\Mr Oscar Brown.jfif","images\\Mr Hugo Schmidt.jfif","images\\Mr Blake Sanders.jfif","images\\Mr Toby Beattie.jfif","images\\Mr Christopher Myers.jfif","images\\Mr Gus Shearer-Smith.jfif","images\\Mr Jake Porter.jfif","images\\Mr Angus Ferrett.jfif","images\\Mr Leo Callaghan.jfif","images\\Mr Samuel Welsh.jfif","images\\Mr Josh Lord.jfif","images\\Mr Harry Wong.jfif","images\\Mr Alex Holmes.jfif","images\\Mr George Prineas.jfif","images\\Mr Louis Rhodes.jfif","images\\Mr William Horvat.jfif","images\\Mr Cameron Philip.jfif","images\\Mr Ned McCartney.jfif","images\\Mr Harrison Whitting.jfif","images\\Mr Hugh McMaster.jfif","images\\Mr Oliver Wood.jfif","images\\Mr Thomas White.jfif","images\\Mr Jacob Williams.jfif","images\\Mr Hugo Forster.jfif","images\\Mr Sebastian Buchner.jfif","images\\Mr Hamish Ward.jfif","images\\Mr Oliver Davies.jfif","images\\Mr Sam Nott.jfif","images\\Mr Toby Garraway.jfif","images\\Mr Isaac Liggett.jfif","images\\Mr Benjamin Simonds.jfif","images\\Mr Zach McAdam.jfif","images\\Mr Benjamin Rankin.jfif","images\\Mr Daniel Coghlan.jfif","images\\Mr Will Clauson.jfif","images\\Mr Samuel Goodman.jfif","images\\Mr Lachlan White.jfif","images\\Mr Hugh Cotton.jfif","images\\Mr Alex Boardman.jfif","images\\Mr Lucas Wade.jfif","images\\Mr Angus MacDowell.jfif","images\\Mr Henry Duhig.jfif","images\\Mr Joseph Stoddart.jfif","images\\Mr William Brett.jfif","images\\Mr Angus McPherson.jfif","images\\Mr Henry Page.jfif","images\\Mr Patrick Gearing.jfif","images\\Mr Balin Ahern.jfif","images\\Mr MacKenzie Jolly.jfif","images\\Mr George Batkin.jfif","images\\Mr Baxter Schmidt.jfif","images\\Mr Jett May.jfif","images\\Mr Liam Dunn.jfif","images\\Mr Andrew Elsdon.jfif","images\\Mr Nicholas Lacheca.jfif","images\\Mr Lachlan Dunks.jfif","images\\Mr Adam Goedecke.jfif","images\\Mr Hunter Devaney.jfif","images\\Mr Billy Jones.jfif","images\\Mr Jordan Patton.jfif","images\\Mr Benjamin Joslin.jfif","images\\Mr Edward Newell.jfif","images\\Mr Zach Smith.jfif","images\\Mr Hugo Dart.jfif","images\\Mr Stanton Clark.jfif","images\\Mr Ned Hindman.jfif","images\\Mr Dalton Nyholt.jfif","images\\Mr Joshua Hill.jfif","images\\Mr William Latimer.jfif","images\\Mr Evan Williams.jfif","images\\Mr Patrick Brackenrig.jfif","images\\Mr Lachlan Murdoch.jfif","images\\Mr Max McCarthy.jfif","images\\Mr Edward Storen.jfif","images\\Mr Lachlan Kendall.jfif","images\\Mr Jeremy Lee.jfif","images\\Mr Thomas Pappas.jfif","images\\Mr Will Toovey.jfif","images\\Mr Angus Robbins.jfif","images\\Mr Ben McCann.jfif","images\\Mr Anthony McLaughlin.jfif","images\\Mr Benjamin Renwick.jfif","images\\Mr Sandy Crooke.jfif","images\\Mr Adam Huang.jfif","images\\Mr Tom White.jfif","images\\Mr William Horton.jfif","images\\Mr Jack Christie.jfif","images\\Mr Cooper Haines.jfif","images\\Mr James Morris.jfif","images\\Mr Daniel Rosengren.jfif","images\\Mr Oscar Harris.jfif","images\\Mr Toby Wilson.jfif","images\\Mr Epeli Toua.jfif","images\\Mr Kuzi Harwood.jfif","images\\Mr Josh Hayden.jfif","images\\Mr Joseph Carnavas.jfif","images\\Mr Cooper Winter.jfif","images\\Mr Daniel Frederiksen.jfif","images\\Mr Cameron Middleton.jfif","images\\Mr Mason La Caze.jfif","images\\Mr Ethan Fulcher.jfif","images\\Mr Noah Bickmore.jfif","images\\Mr Hugo McCamley.jfif","images\\Mr Charlie Adam.jfif","images\\Mr Robert Russell.jfif","images\\Mr Lachlan Southwood.jfif","images\\Mr Tom Southwood.jfif","images\\Mr Tom Calder.jfif","images\\Mr Sam Scott.jfif","images\\Mr Lachlan Mills.jfif","images\\Mr Jagroop Randhawa.jfif","images\\Mr Ben Holt.jfif","images\\Mr Riley Cox.jfif","images\\Mr Mitchell O'Hehir.jfif","images\\Mr Jibreel Baytieh.jfif","images\\Mr Louis Christ.jfif","images\\Mr Lawson Heraghty.jfif","images\\Mr Tim Chandler.jfif","images\\Mr Caleb Plasto-Chalk.jfif","images\\Mr Charlie Johnstone.jfif","images\\Mr Luke Burns.jfif","images\\Mr Daniel Le.jfif","images\\Mr Angus Montague.jfif","images\\Mr Archie Smith.jfif","images\\Mr Patrick Skelton.jfif","images\\Mr Kevin X Wang.jfif","images\\Mr George Murphy.jfif","images\\Mr Jack Crethary.jfif","images\\Mr Ryan Kim.jfif","images\\Mr Digby Fysh.jfif","images\\Mr Hugo Brown.jfif","images\\Mr William Zhai.jfif","images\\Mr Leo Zhai.jfif","images\\Mr Jason Sia.jfif","images\\Mr Toby Hunt.jfif","images\\Mr Ollie Burnham.jfif","images\\Mr Joshua Won.jfif","images\\Mr Dominic Patapan.jfif","images\\Mr Thomas Pearce.jfif","images\\Mr Kallen Mattsson.jfif","images\\Mr Clancy Brady.jfif","images\\Mr Billy Fitzpatrick.jfif","images\\Mr Tom Wehl.jfif","images\\Mr James Gordon.jfif","images\\Mr Ricky Rotelli.jfif","images\\Mr Emil Willie-Jawai.jfif","images\\Mr Kye Boland.jfif","images\\Mr Sidney York.jfif","images\\Mr Aditya Ghai.jfif","images\\Mr Lewis Gray.jfif","images\\Mr Frankie Goldsbrough.jfif","images\\Mr Jensen Taafe.jfif","images\\Mr Josh Richardson.jfif","images\\Mr Dylan Thomas.jfif","images\\Mr George Forrest.jfif","images\\Mr Hunter Greenwood.jfif","images\\Mr Hamza Lingo.jfif","images\\Mr Tom Eyres.jfif","images\\Mr Roshan Dahanayake.jfif","images\\Mr Tommi Jacobsen.jfif","images\\Mr Arjun Maharaj.jfif","images\\Mr Drew Carmichael.jfif","images\\Mr Jake Miles-Wrency.jfif","images\\Mr Charlie Moore.jfif","images\\Mr Jacky Xie.jfif","images\\Mr Oliver Po.jfif","images\\Mr Justin Chiang.jfif","images\\Mr Bruce Bickerstaff.jfif","images\\Mr Noah Souter.jfif","images\\Mr Angus Roughan.jfif","images\\Mr Jack Hedges.jfif","images\\Mr Diamond Fifita.jfif","images\\Mr Benson Teljega.jfif","images\\Mr Hayden Goodwin.jfif","images\\Mr Knox Burnham.jfif","images\\Mr Billy Condon.jfif","images\\Mr Max McCartney.jfif","images\\Mr Isaac Barker.jfif","images\\Mr Beau Johnstone.jfif","images\\Mr Caleb Cronn.jfif","images\\Mr Thomas Pattison.jfif","images\\Mr Lachie Booth.jfif","images\\Mr Byron Gray.jfif","images\\Mr Isaac Sellwood.jfif");
+
+
+preload(students.toString())
+
+function start() {
+  var arr = [];
+  while(arr.length < 3){
+    var r = Math.floor(Math.random() * students.length) + 1;
+    if(arr.indexOf(r) === -1) arr.push(r);
+  }
+  var one = students[arr[0]]
+  var two = students[arr[1]]
+  var three = students[arr[2]]
+
+
+  document.getElementById('1').childNodes[1].src = one
+  document.getElementById('1').childNodes[5].innerHTML = one.replace('images\\Mr','').replace('.jfif','');
+  document.getElementById('2').childNodes[1].src = two
+  document.getElementById('2').childNodes[5].innerHTML = two.replace('images\\Mr','').replace('.jfif','');
+  document.getElementById('3').childNodes[1].src = three
+  document.getElementById('3').childNodes[5].innerHTML = three.replace('images\\Mr','').replace('.jfif','');
+
+
+}
+
+
+
+
+rating = 1
+
+function studentClicked(id) {
+document.getElementById(id).childNodes[3].innerHTML = rating;
+
+  document.getElementById(id).onclick = "null";
+rating += 1
+if (rating > 3) {
+  rating = 1
+  start()
+  document.getElementById("1").childNodes[3].innerHTML = "";
+  document.getElementById("1").setAttribute( "onClick", "studentClicked(this.id);" );
+  document.getElementById("2").childNodes[3].innerHTML = "";
+  document.getElementById("2").setAttribute( "onClick", "studentClicked(this.id);" );
+  document.getElementById("3").childNodes[3].innerHTML = "";
+  document.getElementById("3").setAttribute( "onClick", "studentClicked(this.id);" );
+}
+}
+
+
+
+function preload(images) {
+    if (document.images) {
+        var i = 0;
+        var imageArray = new Array();
+        imageArray = images.split(',');
+        var imageObj = new Image();
+        for(i=0; i<=imageArray.length-1; i++) {
+            //document.write('<img src="' + imageArray[i] + '" />');// Write to page (uncomment to check images)
+            imageObj.src=imageArray[i];
+        }
+    }
+}
